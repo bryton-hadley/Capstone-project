91b6ea6c5225cd2b9e3ac464ed30fbd4df921c8c
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-26 15:45:13 -0600

    first commit

---
+++ client/index.js
@@ -0,0 +1,12 @@
+const baseUrl =
+
+
+
+
+//set up axios request to get ski resorts array
+//loop over that array
+//Create Ski resort cards for each item in the array 
+
+const getAllResorts = () => {
+
+}
---
+++ server/controller.js
@@ -0,0 +1,11 @@
+const resorts = require('./db.json')
+
+
+// setting up the get, post, delete, put methods 
+
+module.exports = {
+
+    getTattoos: (req, res) => {
+
+    }
+}
